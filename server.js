b0b4e22603bedbc91edc13e4634a4c00c3ff0cca
Muneeb-Usman <muneebusman54@gmail.com>
2024-01-01 11:22:18 +0500

    added create and get appointment api

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ userDetails: {
 });
 
 const Slot = mongoose.model('Slot', slotSchema);
+const Appointment = mongoose.model('Appointment', appointmentSchema);
 
 app.use(cors());
 app.use(express.json());
@@ -55,7 +56,42 @@ try {
     res.status(500).json({ error: 'Internal Server Error' });
 }
 });
+
+
+// create appointment api
+app.post('/api/appointments', async (req, res) => {
+    try {
+        const { slotId, userDetails } = req.body;
+        const slot = await Slot.findById(slotId);
+        if (!slot || slot.isBooked) {
+        res.status(400).json({ error: 'Invalid slot or already booked' });
+        return;
+        }
     
+        const newAppointment = new Appointment({ slotId, userDetails });
+        await newAppointment.save();
+    
+        slot.isBooked = true;
+        await slot.save();
+    
+        res.status(201).json(newAppointment);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
+// get all appointments
+app.get('/api/appointments', async (req, res) => {
+try {
+    const appointments = await Appointment.find();
+    res.status(200).json(appointments);
+} catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+}
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
