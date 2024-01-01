42a3b3898ff9995d84d2cd262b9e89e1dd853a39
Muneeb-Usman <muneebusman54@gmail.com>
2024-01-01 11:14:12 +0500

    added schema for slots and appointment in mongodb database

--- server.js
+++ server.js
@@ -10,6 +10,28 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => console.log('Connected to MongoDB'));
 
+
+const slotSchema = new mongoose.Schema({
+    startTime: Date,
+    endTime: Date,
+    isBooked: Boolean,
+  });
+  
+const appointmentSchema = new mongoose.Schema({
+slotId: mongoose.Schema.Types.ObjectId,
+userDetails: {
+    name: String,
+    email: String,
+},
+});
+
+const Slot = mongoose.model('Slot', slotSchema);
+const Appointment = mongoose.model('Appointment', appointmentSchema);
+
+app.use(cors());
+app.use(express.json());
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
